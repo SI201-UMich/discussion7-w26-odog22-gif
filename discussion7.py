304647c2dd88b891e15c7eb146f0f8769e9df325
odog22-gif <ocfues@umich.edu>
2026-02-25 17:38:13 -0500

    finished step 3, still has errors

--- discussion7.py
+++ discussion7.py
@@ -104,7 +104,16 @@ def write_summary_csv(out_filename, avg_prices):
         None
             Writes a CSV file with header: neighbourhood_group, room_type, average_price
     """
-    pass
+
+    with open(out_filename, 'w', newline = '') as fout:
+        writer = csv.writer(fout)
+        writer.writerow(['neighbourhood_group', 'room_type', 'average_price'])
+        
+        for key, value in avg_prices.items():
+            writer.writerow([key[0], key[1], value])
+    
+    return
+    # pass
 
 ###############################################################################
 ##### UNIT TESTS (Do not modify the code below!)
